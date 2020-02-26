cebfb55f3a12509f5d319612b4481cab6d1acb4d
macrusal <macrusal@gmail.com>
2020-02-25 21:32:11 -0300

    calculate handler

--- api-calculate/handler/calculate_handler.go
+++ api-calculate/handler/calculate_handler.go
@@ -7,4 +7,14 @@ import (
 
 func CalculateHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Chegou no handler...")
+
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+
+	fmt.Println("O que tem no r..: ", r.URL)
+	fmt.Println("O que tem no r..: ", r.URL.Path)
+	fmt.Println("O que tem no r..: ", r.URL.Path[len("/calculate/"):])
+	fmt.Println("O que tem no r..: ", r.URL.Path[len("/calculate/"):])
 }
